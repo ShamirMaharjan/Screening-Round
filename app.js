14a26fda13bf50fa3918d44d65f43150fbe986de
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-11-11 11:48:55 +0545

    initial project set up

---
+++ app.js
@@ -0,0 +1,13 @@
+import experess from 'express';
+
+const app = experess();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
